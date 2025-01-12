6e4a6d3b95628c6e81f79df92959cd842db6da6a
xezzon <xezzon@outlook.com>
2025-01-12 16:35:46 +0800

    zeroweb-service-open ts接口。

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,2 +1,4 @@
 export { default as ZerowebAdminCient } from './admin'
 export type { Dict, User } from './admin'
+
+export { default as ZerowebOpenClient } from './open'
---
+++ sdk/src/open/index.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { InstanceConfig } from "../types";
+
+export default (config: InstanceConfig) => {
+  const instance = axios.create(config)
+
+  return {
+    /**
+     * HTTP 客户端
+     */
+    instance,
+  }
+}
