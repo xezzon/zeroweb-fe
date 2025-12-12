6c6a33e53afd9da265a86738677acd718b43aece
xezzon <xezzon@outlook.com>
2025-12-12 22:45:25 +0800

    应用管理 HTTP 接口。

--- sdk/src/admin/index.ts
+++ sdk/src/admin/index.ts
@@ -1,5 +1,6 @@
 import { InstanceConfig } from "@/types"
 import axios from "axios"
+import app from "./app"
 
 export default (config: InstanceConfig) => {
   const instance = axios.create(config)
@@ -9,5 +10,11 @@ export default (config: InstanceConfig) => {
      * 拦截器方法
      */
     interceptors: instance.interceptors,
+    /**
+     * 应用管理相关接口
+     */
+    app: app(instance),
   }
 }
+
+export type * from './app'
