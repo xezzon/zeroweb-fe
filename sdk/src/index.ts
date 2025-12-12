62397b56ce9ff08f3f45bd36900b3686a060a3d6
xezzon <xezzon@outlook.com>
2025-12-12 22:45:09 +0800

    系统管理模块。

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,2 +1,5 @@
 export { default as ZerowebMetadataClient, MenuType, ServiceType, } from './metadata'
 export type * from './metadata'
+
+export { default as ZerowebAdminClient } from './admin'
+export type * from './admin'
