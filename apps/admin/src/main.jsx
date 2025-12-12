ada7e93714c2414ee3e40d7583e9eb561b90a57e
xezzon <xezzon@outlook.com>
2025-12-12 23:58:21 +0800

    添加 `系统管理` 应用。

---
+++ apps/admin/src/main.jsx
@@ -0,0 +1,8 @@
+// oxlint-disable-next-line no-unused-vars
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')).render(
+  <>
+  </>,
+)
