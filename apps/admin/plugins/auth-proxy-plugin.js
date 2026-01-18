6af9e492761049cb48f334bac3c96badf349f9cb
xezzon <xezzon@outlook.com>
2026-01-18 10:54:33 +0800

    将 authProxy 改造成插件。

---
+++ apps/admin/plugins/auth-proxy-plugin.js
@@ -0,0 +1,58 @@
+/**
+ * @param {object} options 
+ * @param {string} options.token
+ * @param {string} options.jwk
+ * @param {string[]} options.excludePaths
+ */
+export default function authProxyPlugin(options = {}) {
+  const { 
+    token: idToken,
+    jwk,
+    excludePaths = ['/auth/login/basic'],
+  } = options
+  /**
+   * @param {import('http').ClientRequest} proxyReq 
+   */
+  const onProxyReq = (proxyReq) => {
+    if (excludePaths.includes(proxyReq.path)) {
+      return
+    }
+    if (idToken) {
+      proxyReq.setHeader('Authorization', `Bearer ${idToken}`)
+    }
+    if (jwk) {
+      proxyReq.setHeader('X-PUBLIC-KEY', jwk)
+    }
+  }
+  return {
+    name: 'auth-proxy',
+    /**
+     * 
+     * @param {import('rolldown-vite').UserConfig} config 
+     * @returns 
+     */
+    config(config) {
+      const proxies = config?.server?.proxy
+      if (!proxies) {
+        return config
+      }
+      Object.keys(proxies).forEach(key => {
+        /**
+         * @type {import('rolldown-vite').ProxyOptions}
+         */
+        const proxyConfig = proxies[key]
+        if (proxyConfig.configure) {
+          const originalConfigure = proxyConfig.configure
+          proxyConfig.configure = (proxy) => {
+            originalConfigure(proxy)
+            proxy.on('proxyReq', onProxyReq)
+          }
+        } else {
+          proxyConfig.configure = (proxy) => {
+            proxy.on('proxyReq', onProxyReq)
+          }
+        }
+      })
+    }
+  }
+}
--- apps/admin/vite.config.js
+++ apps/admin/vite.config.js
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { resolve } from "node:path";
 import { defineConfig, loadEnv } from 'rolldown-vite';
+import authProxyPlugin from './plugins/auth-proxy-plugin';
 import metadata from './plugins/metadata-plugin';
 
 const envPrefix = ['ZEROWEB_', 'VITE_']
@@ -10,29 +11,15 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), envPrefix)
   const base = '/'
 
-  /**
-   * 向请求头添加 Token
-   * @param {import('http').ClientRequest} proxyReq 
-   */
-  const authProxy = (proxyReq) => {
-    if (proxyReq.path === '/auth/login/basic') {
-      return;
-    }
-    const idToken = env.VITE_TOKEN;
-    if (idToken) {
-      proxyReq.setHeader('Authorization', `Bearer ${idToken}`)
-    }
-    const publicKey = env.VITE_PUBLIC_KEY;
-    if (publicKey) {
-      proxyReq.setHeader('X-PUBLIC-KEY', publicKey)
-    }
-  }
-
   return {
     base: base,
     envPrefix,
     plugins: [
       react(),
+      authProxyPlugin({
+        token: env.VITE_TOKEN,
+        jwk: env.VITE_JWK,
+      }),
       metadata({
         name: process.env.npm_package_name,
         version: process.env.npm_package_version,
@@ -52,17 +39,11 @@ export default defineConfig(({ mode }) => {
           target: env.VITE_ADMIN_URL,
           changeOrigin: true,
           rewrite: (path) => path.replace(env.ZEROWEB_ADMIN_API, ''),
-          configure: (proxy) => {
-            proxy.on('proxyReq', authProxy)
-          },
         },
         [env.ZEROWEB_OPEN_API]: {
           target: env.VITE_OPEN_URL,
           changeOrigin: true,
           rewrite: (path) => path.replace(env.ZEROWEB_OPEN_API, ''),
-          configure: (proxy) => {
-            proxy.on('proxyReq', authProxy)
-          },
         },
       },
     },
